0d5c0645eb39c8a965d4a9230149b705bde387aa
Aysha Nadeem <ayshan@packt.com>
2026-02-11 13:59:28 +0530

    chore: initial project setup with Terraform workflow

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "staging_bucket" {
+  bucket = "my-staging-bucket-${var.random_suffix}"
+
+  tags = {
+    Name        = "Staging Bucket"
+    ManagedBy   = "Terraform via GitHub Actions"
+  }
+}
+	variables.tf
+variable "aws_region" {
+  description = "The AWS region for our resources."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "random_suffix" {
+  description = "A random suffix to ensure the bucket name is unique."
+  type        = string
+}[
